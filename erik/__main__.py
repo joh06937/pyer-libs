520d54ac90ea3c84bb070487f7d048135df90ab1
Erik Johnson <joh06937@gmail.com>
2025-09-27 10:59:11 -0500

    General: Added the bones for a base repo with a command-line

---
+++ erik/__main__.py
@@ -0,0 +1,19 @@
+"""
+The main entry point for the package
+"""
+
+import sys
+
+def main():
+    """The main entry point
+
+    :param none:
+
+    :return none:
+    """
+
+    # Run commands
+    pass
+
+if __name__ == "__main__":
+    main()
